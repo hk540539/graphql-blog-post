c4b29d338cbc179e363b6d778900b075474e58e8
hk <hk540539@gmail.com>
2022-03-31 16:02:13 +0530

    myprofile check

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -12,9 +12,19 @@ export const Query = {
   profile: async (
     _: any,
     { userId }: { userId: string },
-    { prisma }: Context
+    { prisma, userInfo }: Context
   ) => {
-    return prisma.profile.findUnique({ where: { userId: Number(userId) } });
+    const isMyProfile = Number(userId) === userInfo?.userId;
+    console.log(isMyProfile, "hit");
+    const profile = await prisma.profile.findUnique({
+      where: { userId: Number(userId) },
+    });
+    if (!profile) return null;
+
+    return {
+      ...profile,
+      isMyProfile,
+    };
   },
 
   posts: async (_: any, __: any, { prisma }: Context) => {
